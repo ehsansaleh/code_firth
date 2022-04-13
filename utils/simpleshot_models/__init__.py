99f6e120c1e6bc32004b679fb23e06178e6f2e0f
ehsansaleh <ehsan.slh@gmail.com>
2022-04-12 23:26:32 -0500

    the simpleshot backbone definitions

---
+++ utils/simpleshot_models/__init__.py
@@ -0,0 +1,7 @@
+# credit: https://github.com/mileyan/simple_shot/tree/master/src/models
+
+from .ResNet import *
+from .DenseNet import *
+from .Conv4 import Conv4 as conv4
+from .MobileNet import *
+from .WideResNet import wideres
